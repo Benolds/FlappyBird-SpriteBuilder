1dcde5c12960299f7218a804252bfd23c4bfeb8b
Benjamin Reynolds <reynoldsben94@gmail.com>
2014-06-25 00:44:43 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,11 +11,19 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceLastObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceLastObstacle += delta;
+    
+    if (timeSinceLastObstacle > 2.0f) {
+        [self addObstacle];
+        
+        timeSinceLastObstacle = 0.0f;
+    }
 }
 
 // put new methods here
